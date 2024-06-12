0b273cdd60583034fb5349512bbac8f00633818d
Bruno Guilherme da Silva <121551639+Bruno-Guilherme@users.noreply.github.com>
2024-06-12 03:51:39 +0000

    update

--- js/view/index.js
+++ js/view/index.js
@@ -3,6 +3,11 @@ import './dependencias.js'
 import { render } from './graphs.js';
 
 const abrirSidebar = () => {
+  
+};
+
+const menu = document.getElementById('hamburguer');
+menu.onclick = () => {
   const aside = document.querySelector("aside");
 
   console.log("Abriu")
@@ -17,6 +22,25 @@ const abrirSidebar = () => {
     aside.style.display = "none";
     console.log('none');
   }
-};
+}
+
+const fecharSide = document.getElementById('fechar-side');
+
+fecharSide.onclick = () => {
+  const aside = document.querySelector("aside");
+
+  console.log("Abriu")
+
+  // Verifica se a barra lateral está atualmente visível ou oculta
+  if (aside.style.display === 'none' || aside.style.display === '') {
+    // Se estiver oculta, torna-a visível
+    aside.style.display = "block";
+    console.log("block");
+  } else {
+    // Se estiver visível, oculta-a
+    aside.style.display = "none";
+    console.log('none');
+  }
+}
 
 render();
